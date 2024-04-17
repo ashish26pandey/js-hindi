37ef6dd6b42cc4aedfac04de9f0237212702e4a7
Ashish Pandey <141471554+ashish5231@users.noreply.github.com>
2024-04-17 12:07:40 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using nodejs, not browser       
+
+console.log(3 + 3); // code readability should be high
+
+console.log("ashish");
+
+// datatypes:-----
+// number  :-- 2 to power 54 
+// bigInt
+// string :--- ""
+// boolean :--- true/false
+// null :-- standalone value
+// undefined :---
+// symbol:---- unique
+
+
+// object
+console.log(typeof "ashish");
+
+console.log(typeof null);
+
+console.log(typeof undefined);  undefined// 
