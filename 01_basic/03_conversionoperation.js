d3b7f038856e3d4f5224043518faa1c17ae7a3d1
Ashish Pandey <141471554+ashish26pandey@users.noreply.github.com>
2024-05-03 17:39:05 +0000

    conversion operation

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -11,6 +11,7 @@
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
+// notes:----------------------
 // "33" => 33
 // "33abc" => NaN (not a number) typeof nan is number
 // true => 1;
@@ -23,13 +24,42 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)
 
+// notes:--------------------------
 // 1 => true;
 // 0 => false;
 // "" => false;
 // "ashish" => true;
 
 // :------------------------------------------
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// operations:-------------------------------
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3);
+
+// let str1 = "hello";
+// let str2 = " ashish"
+
+// let str3 = str1 + str2;
+// console.log(str3)
+
+// console.log("1" +2)
+// console.log(1 + "3")
+// console.log(1 + 2 + "3")
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
